7cf6fbc8a23b35a9aa1d23e487fcd4e9cdac92d3
huangyang <840499869@qq.com>
2020-08-13 18:29:03 +0800

    GPUS

--- GPUs.py
+++ GPUs.py
@@ -2,3 +2,5 @@ import tensorflow as tf
 if __name__ == '__main__':
     gpus = tf.config.list_physical_devices(device_type="GPU")
     print("GPU:", gpus)
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    print("GPU:", gpus)
